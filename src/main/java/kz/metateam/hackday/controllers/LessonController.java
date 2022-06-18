6fd080eaa175f55ac2ec5e15cbb59b0a29195001
Bakhytzhan Amanzholov <b.amanzholov@kzn.akvelon.com>
2022-06-18 11:33:27 +0600

    Fourteenth commit

--- src/main/java/kz/metateam/hackday/controllers/LessonController.java
+++ src/main/java/kz/metateam/hackday/controllers/LessonController.java
@@ -1,6 +1,7 @@
 package kz.metateam.hackday.controllers;
 
 import kz.metateam.hackday.models.specialties.Lesson;
+import kz.metateam.hackday.models.specialties.Specialization;
 import kz.metateam.hackday.service.LessonService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 @RestController
 @Slf4j
@@ -27,5 +30,13 @@ public class LessonController {
     public ResponseEntity<?> findAllSpecializationByLesson(@PathVariable("id") Long id){
         Lesson lesson = lessonService.findById(id);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(lesson);
+    }
+    @GetMapping("/{id1}/{id2}")
+    public ResponseEntity<?> findAllSpecializationByLessons(@PathVariable("id1") Long id1, @PathVariable("id2") Long id2){
+        Lesson lesson1 = lessonService.findById(id1);
+        Lesson lesson2 = lessonService.findById(id2);
+        Set<Specialization> specializations = lesson1.getSpecializationSet();
+        specializations.retainAll(lesson2.getSpecializationSet());
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(specializations);
     }
 }
